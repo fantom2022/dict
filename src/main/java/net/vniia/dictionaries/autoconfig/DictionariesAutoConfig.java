21715652561b116ffc150e10f2acfa93ff8e1519
Lexa Vopper <lehandro711@gmail.com>
2022-12-26 14:26:11 +0300

    add javax, add ComponentScan

--- src/main/java/net/vniia/dictionaries/autoconfig/DictionariesAutoConfig.java
+++ src/main/java/net/vniia/dictionaries/autoconfig/DictionariesAutoConfig.java
@@ -1,11 +1,10 @@
 package net.vniia.dictionaries.autoconfig;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EntityScan({"net.vniia.dictionaries.entities", "net.vniia.dictionaries.dto"})
+@ComponentScan({"net.vniia.dictionaries.entities", "net.vniia.dictionaries.dto"})
 @ComponentScan("net.vniia.dictionaries")
 public class DictionariesAutoConfig {
 }
